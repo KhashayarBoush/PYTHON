f248e3eaee59d3ccc1fd935b4c0af55d066bff00
khashayarboush <khashayar.boush@icloud.com>
2019-10-16 03:13:34 +0330

    Add new File
    Slicer
    Stings

---
+++ Slicer.py
@@ -0,0 +1,15 @@
+# use Slicer For give part of date
+name = 'khashayar'
+print(name[0])
+print(name[0:5])
+print(name[:] + str(""" this is Slicer And 3Pel Qoute . . . """))
+
+# we can use Steper 
+about = "this is about of me"
+print(about[0:20:5]) # [start : end : step ]
+
+# Give Last Chrachter
+wtf = "Good"
+print(wtf[-1])
+print(wtf[-2:])
+print(wtf[:-3])
---
+++ Strings.py
@@ -0,0 +1,11 @@
+# use String data type For Stor Charchter Data
+name = 'khashayar'
+family = 'boush'
+ 
+# use  threepel " "  
+About = """
+i'm khashayar boush 
+python developer
+
+"""
+

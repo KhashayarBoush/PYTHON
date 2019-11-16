a6ec9f47526fb90b1b2569882c55e21ad0bddc57
khashayarboush <khashayar.boush@icloud.com>
2019-11-16 19:38:30 +0330

    add Dictionary.py file

---
+++ 12-Dictionary.py
@@ -0,0 +1,8 @@
+# use Dictionary like as JSON file
+
+Dic = {"name"   : "khashayar",
+       "family" : "boush",
+       "age"    : "21",
+       "type"   : "A" }
+
+print ("my name is : " + Dic["name"] + " And My family is : " + Dic["family"] + " im " + Dic["age"] + " years Old")

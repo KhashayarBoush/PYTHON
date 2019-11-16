b5390543f8a2887261d762d76d02276d54f10f57
khashayarboush <khashayar.boush@icloud.com>
2019-11-16 20:27:00 +0330

    change function

---
+++ 13-User-defined-functions.py
@@ -0,0 +1,11 @@
+# do you think need a text code evry day
+# youn write this every day ?
+# no
+# so we can wrire just once and use billions
+# for careate functions we lan free 2 line space of new file
+
+
+def my_func():
+    print("this is a python func test")
+
+my_func()

c62b3a0aeb454e0514df2a3cc9b7392419760b3f
khashayarboush <khashayar.boush@icloud.com>
2019-11-16 19:59:26 +0330

    change variable.py

--- 3-variables.py
+++ 3-variables.py
@@ -9,4 +9,19 @@
 name = str (" khashayar ")
 family = str ( " boush " )
 age = int (21)
-print("i am " + name + family + " and im " + str(age) + " years Old . ")
+number = int(9397582495)
+
+
+result = str("i am " + name + family + " and im " + str(age) + " years Old And my phone number is : " + str(number))
+print("=" * 20)
+print(result)
+print("=" * 20)
+print(result.split(":"))
+print("=" * 20)
+print(result.upper())
+print("=" * 20)
+print(result.lower())
+print("=" * 20)
+print(result.capitalize())
+print("=" * 20)
+
